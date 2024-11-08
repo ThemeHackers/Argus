0c1443dd2877cca459ac1af1fd2fbc1c6fe2b280
ds_zct <107047730+ThemeHackers@users.noreply.github.com>
2024-11-09 02:09:20 +0700

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,15 @@
+from flask import Flask
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+import os
+
+def create_app():
+   
+    app = Flask(__name__)
+
+    app.config.from_pyfile(os.path.join(os.path.dirname(__file__), 'config/settings.py'))
+ 
+    limiter = Limiter(get_remote_address, app=app, default_limits=["200 per day", "50 per hour"])
+
+    return app
 

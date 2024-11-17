85e3422a7efc09454a8dcafb174e1e2fcb8ac999
ds_zct <107047730+ThemeHackers@users.noreply.github.com>
2024-11-17 19:22:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -199,6 +199,4 @@ def internal_error(error):
     return render_template('error.html', error_message="An unexpected error occurred on the server."), 500
 
 if __name__ == '__main__':
-    app.run(port=10000, debug=False)
-
-
+    app.run(port=10000)
